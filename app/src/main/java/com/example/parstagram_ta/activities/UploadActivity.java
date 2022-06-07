438bb140ae9b3f703a57cb4ed4477fc35b144abb
fooledyouonce <emilycrowl2k@gmail.com>
2022-06-07 15:06:03 -0700

    started upload pfp

---
+++ app/src/main/java/com/example/parstagram_ta/activities/UploadActivity.java
@@ -0,0 +1,156 @@
+package com.example.parstagram_ta.activities;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.parstagram_ta.R;
+import com.example.parstagram_ta.models.User;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.File;
+
+public class UploadActivity extends AppCompatActivity {
+    private static final String TAG = "UploadActivity";
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 396;
+    Button btnPfp;
+    Button btnSubmitPfp;
+    ImageView ivPfp;
+    private File photoFile;
+    public String photoFileName = "pfp.jpg";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+
+        btnPfp = findViewById(R.id.btnTakePfp);
+        btnSubmitPfp = findViewById(R.id.btnSubmitPfp);
+        ivPfp = findViewById(R.id.ivPfp);
+
+        btnPfp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchCamera();
+            }
+        });
+
+        btnSubmitPfp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                savePost(currentUser, photoFile);
+                Intent i = new Intent(UploadActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+
+    private void launchCamera() {
+        //create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        //wrap File object into a content provider
+        //required for API >= 24
+        //see https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        //if you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        //so as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(this.getPackageManager()) != null) {
+            //start the image capture intent to take photo
+            //noinspection deprecation
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                //by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                //RESIZE BITMAP, see section below
+                //load the taken image into a preview
+                ivPfp.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Error taking picture!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private File getPhotoFileUri(String photoFileName) {
+        //get safe storage directory for photos
+        //use `getExternalFilesDir` on Context to access package-specific directories.
+        //this way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(this.getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        //create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+            Log.d(TAG, "Failed to create directory");
+        }
+        //return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + photoFileName);
+    }
+
+    private void savePost(ParseUser currentUser, File photoFile) {
+        User pfp = new User();
+        pfp.setKeyPfp(new ParseFile(photoFile));
+        pfp.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Error while saving");
+                    Toast.makeText(UploadActivity.this, "Error while saving!", Toast.LENGTH_SHORT).show();
+                }
+                Log.i(TAG, "Saved post");
+                ivPfp.setImageResource(0);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.cancel, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.cancel) {
+            goTimelineActivity();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void goTimelineActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish(); //makes main activity the "default" page, closes login activity for access
+    }
+}
